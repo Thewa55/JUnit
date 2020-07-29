5d5066935570c9c704c019a05d2dbec0559c8adf
Kenny Lam <kinwai.lam730@gmail.com>
2020-07-28 20:35:32 -0400

    Added before/after annotations

--- src/main/java/TestCase2/Case2Test.java
+++ src/main/java/TestCase2/Case2Test.java
@@ -1,8 +1,33 @@
 package TestCase2;
-import org.junit.Test;
+import org.junit.*;
+
 import static org.junit.Assert.*;
+import static org.junit.AfterClass.*;
+import static org.junit.After.*;
+import static org.junit.BeforeClass.*;
+import static org.junit.Before.*;
 
 public class Case2Test {
+    @AfterClass
+    public static void setupAfterClass() throws Exception{
+        System.out.println("After Class");
+    }
+
+    @After
+    public void setupAfterTest() throws Exception{
+        System.out.println("After Test");
+    }
+
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception{
+        System.out.println("Before Class");
+    }
+
+    @Before
+    public void setUpBeforeTest() throws Exception{
+        System.out.println("Before Test");
+    }
+
     @Test
     public void testFindMax1(){
         assertEquals(4, Case2.findMax1(new int[]{1,3,4,2}));
