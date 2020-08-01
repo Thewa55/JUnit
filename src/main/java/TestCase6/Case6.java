7efbc9984049f8c4a2ecf055532f6613160045cc
Kenny Lam <kinwai.lam730@gmail.com>
2020-08-01 13:33:39 -0400

    Added a new test case

---
+++ src/main/java/TestCase6/Case6.java
@@ -0,0 +1,8 @@
+package TestCase6;
+
+public class Case6 {
+
+    float add(float a, float b){
+        return a + b;
+    }
+}
---
+++ src/main/java/TestCase6/Case6Test.java
@@ -0,0 +1,40 @@
+package TestCase6;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assumptions.*;
+
+public class Case6Test {
+
+    Case6 case6;
+    boolean bool;
+
+    @BeforeEach
+    void init(){
+        System.out.println("Before each");
+        bool = false;
+        case6 = new Case6();
+    }
+
+    @Test
+    void additionTest1(){
+        assumeTrue(bool, "Assumption isn't true -> Test aborted");
+        System.out.println("Inside Test1");
+        assertAll(
+                () -> assertEquals(2, case6.add(1,1), "Doesn't add positive numbers up properly"),
+                () -> assertEquals(-2, case6.add(1,1), "Doesn't add negative numbers up properly")
+        );
+    }
+
+    @Test
+    void additionTest2(){
+        assumeFalse(bool, "Assumption isn't true -> Test aborted");
+        System.out.println("Inside Test1");
+        assertAll(
+                () -> assertEquals(2, case6.add(1,1), "Doesn't add positive numbers up properly"),
+                () -> assertEquals(-2, case6.add(-1,-1), "Doesn't add negative numbers up properly")
+        );
+    }
+}
